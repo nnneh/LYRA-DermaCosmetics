ef0df8c82a0cb29325834c2dd283ae54f3e11f14
Neha Thakur <tneha6260@gmail.com>
2025-12-12 23:54:43 +0545

    created user route and added dotenv file

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express'
+import connect from './db/connection.js'
+import cors from 'cors'
+import userRouter from './routes/user.js'
+import dotenv from 'dotenv'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dotenv.config()
+const port = process.env.PORT
+const app = express()
+
+connect()
+app.use(cors())
+app.use(express.json()) 
+
+app.use(userRouter)
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
